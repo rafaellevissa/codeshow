fc764cd57c5917a1bc2ac67db1813a07be1695e1
rafaellevissa <rafaellevissa@gmail.com>
2020-07-28 22:21:40 -0300

    :bug: banco credentials

--- src/config/db.py
+++ src/config/db.py
@@ -1,9 +1,9 @@
 import MySQLdb
 
 class Db(object):
-    host = "localhost"
-    user = "levi"
-    password = "123"
+    host = "database-1.czlhpvhotuj3.us-east-2.rds.amazonaws.com"
+    user = "campus"
+    password = "Ardu1nohub"
     def __init__(self):
         try:
             self.db = MySQLdb.connect(self.host, self.user, self.password)
